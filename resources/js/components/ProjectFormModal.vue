856f2177bd61cdbb37779c368fd8fb4110c4bb4a
matt <matthewjohncoleman@gmail.com>
2023-07-31 23:30:00 -0500

    format code

--- resources/js/components/ProjectFormModal.vue
+++ resources/js/components/ProjectFormModal.vue
@@ -4,7 +4,8 @@
 
         <div class="fixed inset-0 z-10 overflow-y-auto">
             <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div
+                    class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                     <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                         <form>
                             <div class="space-y-12">
@@ -16,19 +17,24 @@
                                         <div class="sm:col-span-6">
                                             <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
                                             <div class="mt-2">
-                                                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                                    <input v-model="name" type="text" name="name" id="name"  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="New Laravel Project">
+                                                <div
+                                                    class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                    <input v-model="name" type="text" name="name" id="name"
+                                                           class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                           placeholder="New Laravel Project">
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="sm:col-span-6">
                                             <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Path</label>
                                             <div class="mt-2">
-                                                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                <div
+                                                    class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                                     <input :value="path" disabled placeholder="/storage/logs"
                                                            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                                     >
-                                                    <button @click.prevent="openFileDialog" v-if="!path">Choose Folder</button>
+                                                    <button @click.prevent="openFileDialog" v-if="!path">Choose Folder
+                                                    </button>
                                                 </div>
                                             </div>
                                         </div>
@@ -37,10 +43,14 @@
                             </div>
 
                             <div class="mt-6 flex items-center justify-end gap-x-6">
-                                <button type="button" class="text-sm font-semibold leading-6 text-gray-900" @click="close">Cancel</button>
-                                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    @click.prevent="addProject"
-                                >Save</button>
+                                <button type="button" class="text-sm font-semibold leading-6 text-gray-900"
+                                        @click="close">Cancel
+                                </button>
+                                <button type="submit"
+                                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                        @click.prevent="addProject"
+                                >Save
+                                </button>
                             </div>
                         </form>
 
@@ -53,7 +63,7 @@
 </template>
 
 <script setup>
-import { ref, defineEmits } from 'vue'
+import {ref, defineEmits} from 'vue'
 
 const name = ref('')
 const path = ref('')
--- resources/js/components/ProjectTable.vue
+++ resources/js/components/ProjectTable.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden min-h-[600px]">
         <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-            <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                <button type="button" class="ml-auto flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-                    @click="showProjectFormModal = true"
+            <div
+                class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                <button type="button"
+                        class="ml-auto flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                        @click="showProjectFormModal = true"
                 >
-                    <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+                    <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20"
+                         xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path clip-rule="evenodd" fill-rule="evenodd"
+                              d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
                     </svg>
                     Add product
                 </button>
@@ -25,15 +29,18 @@
                 </thead>
                 <tbody>
                 <tr class="border-b dark:border-gray-700" v-for="(project, index) in projects" :key="index">
-                    <td class="px-4 py-3">{{project.name}}</td>
-                    <td class="px-4 py-3">{{project.path}}</td>
+                    <td class="px-4 py-3">{{ project.name }}</td>
+                    <td class="px-4 py-3">{{ project.path }}</td>
                     <td class="px-4 py-3 flex items-center justify-end">
-                        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="apple-imac-27-dropdown-button">
+                        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="apple-imac-27-dropdown-button">
                             <li>
-                                <a :href="`/project/${project.id}`" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-green-600">Show</a>
+                                <a :href="`/project/${project.id}`"
+                                   class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-green-600">Show</a>
                             </li>
                             <li>
-                                <a href="#" @click.prevent="deleteProject(project.id)" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-red-600">Delete</a>
+                                <a href="#" @click.prevent="deleteProject(project.id)"
+                                   class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 text-red-600">Delete</a>
                             </li>
                         </ul>
                     </td>
@@ -45,7 +52,8 @@
             </table>
         </div>
     </div>
-    <ProjectFormModal v-if="showProjectFormModal" @close="showProjectFormModal = false" @addProject="getProjects"></ProjectFormModal>
+    <ProjectFormModal v-if="showProjectFormModal" @close="showProjectFormModal = false"
+                      @addProject="getProjects"></ProjectFormModal>
 </template>
 
 <script setup>
