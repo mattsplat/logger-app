2ea7f5bcad41c8567fa373f21342c80d77c4fd5a
matt <matthewjohncoleman@gmail.com>
2023-08-01 00:26:54 -0500

    fix typo

--- resources/js/components/ProjectTable.vue
+++ resources/js/components/ProjectTable.vue
@@ -12,7 +12,7 @@
                         <path clip-rule="evenodd" fill-rule="evenodd"
                               d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"/>
                     </svg>
-                    Add product
+                    Add Project
                 </button>
             </div>
         </div>
